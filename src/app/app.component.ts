740d787efc1cfc7c94d2f8294a261b61c7afd8cd
spatialpete <shermanps@gmail.com>
2025-04-09 09:02:05 -0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [HeaderComponent, UserComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  users = DUMMY_USERS;
+
+  onSelectUser(id: string) {
+    console.log('selected user with id ' + id)
+  }
+}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, computed, input, Output, EventEmitter } from '@angular/core';
+
+//import { DUMMY_USERS } from '../dummy-users';
+
+//const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({required: true}) id!: string;
+ @Input({required: true}) avatar!: string;
+ @Input({required: true}) name!: string;
+ @Output() select = new EventEmitter();
+
+ get imagePath() {
+  return 'assets/users/' + this.avatar;
+ }
+
+  onSelectUser() {
+   this.select.emit(this.id);
+  }
+}
